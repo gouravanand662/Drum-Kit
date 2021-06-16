b2d1cad4ca605270bfd2304c730e032a017eb757
gouravanand662 <81240130+gouravanand662@users.noreply.github.com>
2021-06-16 13:13:42 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,105 @@
+for(var i=0; i<document.getElementsByTagName("button").length; i++) {
+  document.getElementsByTagName("button")[i].addEventListener("click", function() {
+    var buttoninnerHTML = this.innerHTML;
+
+
+    switch (buttoninnerHTML) {
+        case "w":
+          var tom1 = new Audio('sounds/tom-1.mp3');
+          tom1.play();
+        break;
+
+        case "a":
+          var tom2 = new Audio('sounds/tom-2.mp3');
+          tom2.play();
+        break;
+
+        case "s":
+          var tom3 = new Audio('sounds/tom-3.mp3');
+          tom3.play();
+        break;
+
+        case "d":
+          var tom4 = new Audio('sounds/tom-4.mp3');
+          tom4.play();
+        break;
+
+        case "j":
+          var crash = new Audio('sounds/crash.mp3');
+          crash.play();
+        break;
+
+        case "k":
+          var kickbass = new Audio('sounds/kick-bass.mp3');
+          kickbass.play();
+        break;
+
+        case "l":
+          var snare = new Audio('sounds/snare.mp3');
+          snare.play();
+        break;
+
+      default:
+          console.log(buttoninnerHTML);
+
+    }
+
+        buttonAnimation(buttoninnerHTML);
+  });
+}
+
+document.addEventListener("keydown", function(event) {
+
+  switch (event.key) {
+    case "w":
+      var tom1 = new Audio('sounds/tom-1.mp3');
+      tom1.play();
+    break;
+
+    case "a":
+      var tom2 = new Audio('sounds/tom-2.mp3');
+      tom2.play();
+    break;
+
+    case "s":
+      var tom3 = new Audio('sounds/tom-3.mp3');
+      tom3.play();
+    break;
+
+    case "d":
+      var tom4 = new Audio('sounds/tom-4.mp3');
+      tom4.play();
+    break;
+
+    case "j":
+      var crash = new Audio('sounds/crash.mp3');
+      crash.play();
+    break;
+
+    case "k":
+      var kickbass = new Audio('sounds/kick-bass.mp3');
+      kickbass.play();
+    break;
+
+    case "l":
+      var snare = new Audio('sounds/snare.mp3');
+      snare.play();
+    break;
+
+  default:
+      console.log(event.key);
+
+}
+
+  buttonAnimation(event.key);
+});
+
+function buttonAnimation(currentKey) {
+
+  var activeButton = document.querySelector("." + currentKey);
+  activeButton.classList.add("pressed");
+
+  setTimeout(function () {
+    activeButton.classList.remove("pressed");
+  }, 100);
+}
